02e5146bbe34d73ffb403e41e6811545f84b36fd
Venky <venkatramanm@gmail.com>
2025-03-20 00:06:23 +0530

    Added providerid reference on user

---
+++ src/main/java/in/succinct/bpp/shell/db/model/User.java
@@ -0,0 +1,19 @@
+package in.succinct.bpp.shell.db.model;
+
+import com.venky.swf.db.Database;
+import com.venky.swf.db.annotations.column.IS_NULLABLE;
+import com.venky.swf.db.annotations.column.UNIQUE_KEY;
+
+public interface User extends com.venky.swf.plugins.collab.db.model.user.User {
+    @IS_NULLABLE
+    @UNIQUE_KEY(value = "PROVIDER_ID", allowMultipleRecordsWithNull = true)
+    String getProviderId();
+    void setProviderId(String providerId);
+    
+    public static User findProvider(String providerId){
+        User user  = Database.getTable(User.class).newRecord();
+        user.setProviderId(providerId);
+        user = Database.getTable(User.class).getRefreshed(user);
+        return user;
+    }
+}
